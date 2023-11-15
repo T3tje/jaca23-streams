e35cd8e4f80793c4bdf35d991cb8e9615ba10f5a
T3tje <tilman2013@gmail.com>
2023-11-15 15:56:57 +0100

    add streams

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,10 +8,35 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(1,2,3,4,5,6,7,8,9,10);
 
+        // EVEN NUMBERS
         List<Integer> evenNumbers = numbers.stream()
                 .filter(number -> number % 2 == 0)
                 .toList();
 
         System.out.println(evenNumbers);
+
+        //DOUBLE NUMBERS
+        List<Integer> doubleNumbers = evenNumbers.stream()
+                .map(number -> number * 2)
+                .toList();
+
+        System.out.println(doubleNumbers);
+
+        //REVERSE ORDER LIST
+        List<Integer> reverseOrderList = doubleNumbers.stream()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+
+        System.out.println(reverseOrderList);
+
+        //REDUCE FOR ADDITION
+        int sum = reverseOrderList.stream()
+                .reduce(0, (x,y) -> x+y);
+
+        System.out.println(sum);
+
+        // FOR EACH TO OUTPUT EACH NUMBER
+        reverseOrderList
+                .forEach(System.out::println);
     }
 }
